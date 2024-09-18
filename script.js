5a7a7430e4476c49d325dcb5d6aaf72322502a8f
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-09-18 12:48:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  
+  get width() {
+    return this._width;
+  }
+
+  
+  get height() {
+    return this._height;
+  }
+
+  
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+
+class Square extends Rectangle {
+  constructor(side) {
+    // Call the Rectangle constructor with side as both width and height
+    super(side, side);
+  }
+
+  
+  getPerimeter() {
+    return 4 * this.width; // or 4 * this.height since width = height in a square
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
